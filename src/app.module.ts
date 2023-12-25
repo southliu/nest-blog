29f61b227ee58af8bfbbb8c03fe7ad9c413e783f
southliu <1275093225@qq.com>
2023-12-25 23:21:06 +0800

    wip: redis

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { PostsModule } from './posts/posts.module';
 import { UserModule } from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user/entities/user.entity';
+import { createClient } from 'redis';
 
 @Module({
   imports: [
@@ -23,6 +24,21 @@ import { User } from './user/entities/user.entity';
     UserModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: 'REDIS_CLIENT',
+      async useFactory() {
+        const client = createClient({
+          socket: {
+            host: 'localhost',
+            port: 6379,
+          },
+        });
+        await client.connect();
+        return client;
+      },
+    },
+  ],
 })
 export class AppModule {}
